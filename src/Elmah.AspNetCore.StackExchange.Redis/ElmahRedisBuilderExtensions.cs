9170f8c7143f3cad9a9a2c19898bc5db2706bcc1
Joshua Searles <bleufunk@comcast.net>
2024-03-19 23:12:00 -0400

    feat: add redis factory to options (#55)
    Co-authored-by: Joshua Searles <jsearles@plex.com>

--- src/Elmah.AspNetCore.StackExchange.Redis/ElmahRedisBuilderExtensions.cs
+++ src/Elmah.AspNetCore.StackExchange.Redis/ElmahRedisBuilderExtensions.cs
@@ -14,7 +14,7 @@ public static class ElmahRedisBuilderExtensions
     /// using DI (dependency injection). Options will be defaults unless configured through DI.
     /// </summary>
     /// <param name="builder">The Elmah builder</param>
-    public static void PersistToRedis(this IElmahBuilder builder) => builder.PersistToRedis(o => { });
+    public static void PersistToRedis(this IElmahBuilder builder) => builder.PersistToRedis(static o => { });
 
     /// <summary>
     /// Configures Elmah to use Redis for the error persistence. This will use an instance of <see cref="IConnectionMultiplexer"/> 
@@ -25,7 +25,19 @@ public static class ElmahRedisBuilderExtensions
     public static void PersistToRedis(this IElmahBuilder builder, Action<RedisErrorLogOptions> configureOptions)
     {
         builder.Services.Configure(configureOptions);
-        builder.PersistTo<RedisErrorLog>();
+        builder.PersistTo(static sp =>
+        {
+            var options = sp.GetRequiredService<IOptions<RedisErrorLogOptions>>();
+            if (options.Value.ConnectionMultiplexerFactory is not null)
+            {
+                return new RedisErrorLog(options);
+            }
+
+            return new RedisErrorLog(
+                sp.GetRequiredService<IConnectionMultiplexer>(),
+                sp.GetRequiredService<IOptions<RedisErrorLogOptions>>(),
+                sp.GetService<ILogger<RedisErrorLog>>() ?? NullLogger<RedisErrorLog>.Instance);
+        });
     }
 
     /// <summary>
@@ -34,7 +46,7 @@ public static class ElmahRedisBuilderExtensions
     /// </summary>
     /// <param name="builder">The Elmah builder</param>
     /// <param name="server">The Redis connection</param>
-    public static void PersistToRedis(this IElmahBuilder builder, IConnectionMultiplexer server) => builder.PersistToRedis(server, o => { });
+    public static void PersistToRedis(this IElmahBuilder builder, IConnectionMultiplexer server) => builder.PersistToRedis(server, static o => { });
 
     /// <summary>
     /// Configures Elmah to use Redis for error persistence. The provided <see cref="IConnectionMultiplexer"/> will be used for
